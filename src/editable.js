8370dc58d3cfb1c0f9fc571502ac04e5eacdc020
shiweing <shiweing1998@gmail.com>
2018-10-17 10:06:09 +0800

    Todo with editable js
    Added index.js & editable js with editable list

---
+++ src/editable.js
@@ -0,0 +1,68 @@
+import React from "react";
+import { render } from "react-dom";
+import "./styles.css";
+
+class EditableList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      editable: false
+    };
+  }
+
+  edit = event => {
+    event.persist();
+    this.setState(
+      {
+        editing: true
+      },
+      () => event.target.focus()
+    );
+  };
+
+  save = event => {
+    event.persist();
+    this.setState(
+      {
+        editing: false
+      },
+      () => this.props.edit(event.target.id, event.target.textContent)
+    );
+  };
+
+  handleKeyDown = event => {
+    const { key } = event;
+    switch (key) {
+      case "Enter":
+      case "Escape":
+        this.save(event);
+        break;
+    }
+  };
+
+  render() {
+    const { editing } = this.state;
+    return (
+      <ul>
+        {this.props.todos.map((todo, index) => (
+          <li>
+            <span
+              id={index}
+              onClick={this.edit}
+              contentEditable={editing}
+              onBlur={this.save}
+              onKeyDown={this.handleKeyDown}
+            >
+              {todo}{" "}
+            </span>
+            <button id={index} onClick={this.props.delete}>
+              X
+            </button>
+          </li>
+        ))}
+      </ul>
+    );
+  }
+}
+
+export default EditableList;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,64 @@
+// Referencing https://medium.com/@aghh1504/1-simple-react-todo-list-52186b62976b
 import React from "react";
 import ReactDOM from "react-dom";
+import EditableList from "./editable.js";
 
 import "./styles.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      new_todo: "",
+      todos: []
+    };
+  }
+
+  onChange = event => {
+    this.setState({ new_todo: event.target.value });
+  };
+
+  onSubmit = event => {
+    event.preventDefault();
+    this.setState({
+      new_todo: "",
+      todos: [...this.state.todos, this.state.new_todo]
+    });
+  };
+
+  todos_edit = (index, edited_todo) => {
+    var new_todos = this.state.todos;
+    new_todos[index] = edited_todo;
+    this.setState({
+      todos: new_todos
+    });
+  };
+
+  onDelete = event => {
+    var new_todos = this.state.todos;
+    new_todos.splice(event.target.id, 1);
+
+    this.setState({
+      todos: new_todos
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>TODOS</h1>
+        <form className="App" onSubmit={this.onSubmit}>
+          <input value={this.state.new_todo} onChange={this.onChange} />
+          <button>Submit</button>
+        </form>
+        <EditableList
+          todos={this.state.todos}
+          delete={this.onDelete.bind(this)}
+          edit={this.todos_edit.bind(this)}
+        />
+      </div>
+    );
+  }
 }
 
 const rootElement = document.getElementById("root");
